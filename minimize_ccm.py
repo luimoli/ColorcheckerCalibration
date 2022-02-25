48332be09ac6d4276a1bd6cb6aa6b28cefc14cc5
luimoli <liumm97@163.com>
2022-02-25 17:14:09 +0800

    feat: add ccm calculation mode

--- minimize_ccm.py
+++ minimize_ccm.py
@@ -57,17 +57,17 @@ def ccm(img, ccm):
 #     res_lab = clab.cpu().numpy()
 #     return res_lab
 
-def rgb2lab(img,whitepoint='D65'): 
+def rgb2lab(img, whitepoint='D65'): 
     if (img.ndim==3):
         if (img.shape[2]==3):
-            (rgb,func_reverse)=im2vector(img)
+            (rgb, func_reverse)=im2vector(img)
     elif (img.ndim==2):
         if (img.shape[1]==3):
             rgb=img
             func_reverse=lambda x : x
         elif (img.shape[0]>80)&(img.shape[1]>80):
             img=np.dstack((img,img,img))
-            (rgb,func_reverse)=im2vector(img)
+            (rgb, func_reverse)=im2vector(img)
     rgb=rgb.transpose()
     # rgb=gamma_reverse(rgb,colorspace='sRGB')
     xyz=M_rgb2xyz@rgb
@@ -77,27 +77,37 @@ def rgb2lab(img,whitepoint='D65'):
     lab_tensor = smv_colour.XYZ2Lab(xyz_tensor)
     Lab = lab_tensor.cpu().numpy()
 
-    # f=lambda t : (t>((6/29)**3))*(t**(1/3))+\
-    #     (t<=(6/29)**3)*(29*29/6/6/3*t+4/29)
-    # if whitepoint=='D65':
-    #     Xn = 95.047/100
-    #     Yn = 100/100
-    #     Zn = 108.883/100
-    # L = 116 * f(xyz[:,1]/Yn) - 16
-    # a = 500 * (f(xyz[:,0]/Xn) - f(xyz[:,1]/Yn))
-    # b = 200 * (f(xyz[:,1]/Yn) - f(xyz[:,2]/Zn))
-    # Lab = np.vstack((L,a,b)).transpose()
-
     img_out = func_reverse(Lab)
     return img_out
 
-def ccm_calculate(rgb_data, lab_ideal):
-    """[summary]
+
+def limit(ccm_matrix, threshold=1.5):
+    assert ccm_matrix.shape[0] == ccm_matrix.shape[1] and ccm_matrix.shape[0] == 3
+    for i in range(ccm_matrix.shape[0]):
+        if ccm_matrix[i][i] > threshold:
+            offset = ccm_matrix[i][i] - threshold
+            for j in range(ccm_matrix.shape[1]):
+                if j == i:
+                    ccm_matrix[i][i] = threshold
+                else:
+                    ccm_matrix[i][j] += (offset/2)
+    return ccm_matrix
+
+
+
+def ccm_calculate(rgb_data, lab_ideal, mode='default', optimetric='00'):
+    """[calculate the color correction matrix]
     Args:
         rgb_data ([N*3]): [the RGB data of color_checker]
         lab_ideal ([N*3]): [the ideal value of color_checker]
+        mode (str, optional): [choose how to calculate CCM]. Defaults to 'default'.
+                                default: white balance constrain: the sum of row is 1.
+                                constrain_1.5: constrain the diagonal value to be less than 1.5 when calculating CCM.
+                                limit_1.5: limit CCM's diagonal value after calculation.
+    Raises:
+        ValueError: [mode value error]
     Returns:
-        [nparray]: [shape: 3*3]
+        [array]: [CCM with shape: 3*3]
     """
     x2ccm=lambda x : np.array([[1-x[0]-x[1],x[0],x[1]],
                             [x[2],1-x[2]-x[3],x[3]],
@@ -106,22 +116,42 @@ def ccm_calculate(rgb_data, lab_ideal):
     # f_lab=lambda x : rgb2lab(gamma(ccm(rgb_data,x2ccm(x)),colorspace='sRGB'))
     f_lab=lambda x : rgb2lab(ccm(rgb_data,x2ccm(x)))
 
-    # # --- deltaE 76
-    # f_error=lambda x : f_lab(x)-lab_ideal
-    # f_DeltaE=lambda x : np.sqrt((f_error(x)**2).sum(axis=1,keepdims=True)).mean()
-
-    # # --- deltaE 00
-    f_DeltaE=lambda x : delta_E_CIE2000(f_lab(x), lab_ideal).mean()
+    if optimetric == '76':
+        f_error=lambda x : f_lab(x)-lab_ideal
+        f_DeltaE=lambda x : np.sqrt((f_error(x)**2).sum(axis=1,keepdims=True)).mean()
+    elif optimetric == '00':
+        f_DeltaE=lambda x : delta_E_CIE2000(f_lab(x), lab_ideal).mean()
+    else:
+        raise ValueError(f'optimetric value error!')
 
     x0=np.array([0,0,0,0,0,0])
-    func=lambda x : print('x = ',f_DeltaE(x))
-    result=optimize.minimize(f_DeltaE,x0,method='Powell',callback=func)
-    print(result)
-    print(x2ccm(result.x))
-    pd.DataFrame(x2ccm(result.x)).to_csv('ccm.csv',header=False, index=False)
+    func=lambda x : print('deltaE_00 = ',f_DeltaE(x))
 
-    return x2ccm(result.x)
+    if mode == 'default':
+        result=optimize.minimize(f_DeltaE, x0, method='Powell')
+        print('minimize average deltaE00: ', result.fun)
+        print(x2ccm(result.x))
+        # pd.DataFrame(x2ccm(result.x)).to_csv('ccm.csv',header=False, index=False)
+        return x2ccm(result.x)
 
+    elif mode == 'constrain_1.5':
+        cons = ({'type': 'ineq', 'fun': lambda x: x[0] + x[1] + 0.5},\
+                {'type': 'ineq', 'fun': lambda x: x[2] + x[3] + 0.5},\
+                {'type': 'ineq', 'fun': lambda x: x[4] + x[5] + 0.5})
+        result=optimize.minimize(f_DeltaE, x0, method='SLSQP', constraints=cons)
+        print('minimize average deltaE00: ', result.fun)
+        print(x2ccm(result.x))
+        return x2ccm(result.x)
+
+    elif mode == 'limit_1.5':
+        result=optimize.minimize(f_DeltaE, x0, method='Powell')
+        print('minimize average deltaE00: ', result.fun)
+        limit_matrix = limit(x2ccm(result.x))
+        print(limit_matrix)
+        return limit_matrix
+        
+    else:
+        raise ValueError(f'mode value error!')
 
 
 if __name__ == '__main__':
@@ -129,9 +159,8 @@ if __name__ == '__main__':
     # lab_ideal = np.float32(genfromtxt("./data/real_lab_xrite.csv", delimiter=',')) # from X-rite
     lab_ideal = np.float32(genfromtxt("./data/real_lab.csv", delimiter=',')) # from imatest
     rgb_data = np.float32(genfromtxt("./data/measure_rgb_ck2.csv", delimiter=','))
-    ccm_matrix = ccm_calculate(rgb_data, lab_ideal)
+    ccm_matrix = ccm_calculate(rgb_data, lab_ideal, 'default')
 
     # image_path = r"./img/colorchecker2.jpg"
     # img_convert_wccm(image_path, ccm_matrix)
-
 
